38ac7dc4ee459aa9718fe6be60ddf7ef150b08a9
Enro <antoine.blanchard@gmail.com>
2021-05-01 15:26:24 +0200

    Ajout de consignes

--- Graphs interactifs.R
+++ Graphs interactifs.R
@@ -1,13 +1,11 @@
 library(ggiraph)
 
+# Il convient d'exécuter le fichier Notebook.rmd avant d'exécuter ce script, qui produit des figures interactives grâce à ggiraph
 
-```{r distribution_interactive, results ='asis'}
+# Distribution des fiches métiers en fonction du nombre d'occurrences du terme "données"
 distribution_interactive <- distribution_corps_avec_intitule + geom_point_interactive(aes(tooltip = referens_intitule), size = 2)
 girafe(code = print(distribution_interactive))
-```
 
-
-```{r dispersion_interactive, results ='asis'}
+# Dispersion des fiches métiers autour de la moyenne par BAP
 dispersion_interactive <- dispersion_corps + geom_point_interactive(aes(tooltip = referens_intitule), size = 2)
 girafe(code = print(dispersion_interactive))
-```
